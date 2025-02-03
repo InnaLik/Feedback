4ce9cd82e487a81a488611679d9426ce47e36d11
InnaLik <liksakova@list.ru>
2025-02-03 22:11:47 +0300

    add debug_toolbar

--- product/product/settings.py
+++ product/product/settings.py
@@ -41,8 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'debug_toolbar',
+
     'main',
     'goods',
+
 ]
 
 MIDDLEWARE = [
@@ -53,6 +57,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'product.urls'
@@ -130,3 +136,10 @@ STATICFILES_DIRS = [ BASE_DIR / 'static', ]
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
--- product/product/urls.py
+++ product/product/urls.py
@@ -14,12 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
 
+from product.settings import DEBUG
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("main.urls", namespace='main')),
     path("catalog/", include("goods.urls", namespace='catalog'))
 ]
+
+if DEBUG:
+    urlpatterns += debug_toolbar_urls()
