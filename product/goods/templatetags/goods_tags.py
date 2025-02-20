bda8d2188da8bf5b7030dff4a0033678be19425b
InnaLik <liksakova@list.ru>
2025-02-20 20:19:33 +0300

    filters pagination save

--- product/goods/templatetags/goods_tags.py
+++ product/goods/templatetags/goods_tags.py
@@ -1,3 +1,5 @@
+from urllib.parse import urlencode
+from django.utils.http import urlencode
 from goods.models import Categories
 from django import template
 
@@ -13,3 +15,13 @@ def stars(value):
     full_star = '★'
     empty_star = '☆'
     return f'{full_star * value}{empty_star * (10 - value)}'
+
+# takes_context=True все контекстные переменные будут доступны через context
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    # формируем словарь с данными
+    query = context['request'].GET.dict()
+    # расширяем словарь нашим и значениями в случае с пагинацией это page
+    query.update(kwargs)
+    # urlencode уже формирует параметры в виде строки, которые можно использовать в url адресе
+    return urlencode(query)
