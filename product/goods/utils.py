b27d1ed64908c22ef97153b51fe1ac9318ad1433
InnaLik <liksakova@list.ru>
2025-02-22 20:53:06 +0300

    for search

--- product/goods/utils.py
+++ product/goods/utils.py
@@ -5,5 +5,10 @@ from django.db.models import Q
 def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
-    elif query.isalpha():
-        return Products.objects.filter(name__icontains=query)
+    keywords = [word for word in query.split() if len(word) > 2]
+    q_objects = Q()
+
+    for token in keywords:
+        q_objects |= Q(description__icontains=token)
+
+    return Products.objects.filter(q_objects)
