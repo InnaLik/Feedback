1a58592303251c1d0ea309e8e307086926402202
InnaLik <liksakova@list.ru>
2025-02-22 20:59:27 +0300

    for search

--- product/goods/utils.py
+++ product/goods/utils.py
@@ -1,8 +1,20 @@
+from django.db.models.manager import BaseManager
+
 from goods.models import Products
 from django.db.models import Q
 
 
-def q_search(query):
+def q_search(query) -> BaseManager[Products]:
+    """
+    Возвращает queryset товаров, которые пользователь ввел в строку поиска. Поиск производится по id или
+    имени и названию.
+
+    Args:
+        query: Строка из строки поиска.
+
+    Return:
+        Queryset товаров.
+    """
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
     keywords = [word for word in query.split() if len(word) > 2]
@@ -10,5 +22,6 @@ def q_search(query):
 
     for token in keywords:
         q_objects |= Q(description__icontains=token)
+        q_objects |= Q(name__icontains=token)
 
     return Products.objects.filter(q_objects)
