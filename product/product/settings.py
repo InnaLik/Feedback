65e4ddec1878c4e2ae32003cca64d6e1277ffe95
InnaLik <liksakova@list.ru>
2025-02-23 23:34:49 +0300

    change bd

--- product/product/settings.py
+++ product/product/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'product.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'product',
+        'USER': 'product',
+        'PASSWORD': os.getenv("bd_password"),
+        'HOST': 'localhost',
+        'PORT': '5433'
     }
 }
 
