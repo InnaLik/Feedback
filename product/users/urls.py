3859de7419a05ea1626deade889bf0386bc1fdb6
InnaLik <liksakova@list.ru>
2025-02-26 21:20:16 +0300

    html user add

--- product/users/urls.py
+++ product/users/urls.py
@@ -7,7 +7,7 @@ app_name = 'user'
 
 urlpatterns = [
     path('login/', views.login, name='login'),
-    path('registration/', views.registration, name='reqistration'),
+    path('registration/', views.registration, name='registration'),
     path('profile/', views.profile, name='profile'),
     path('logout/', views.logout, name='logout'),
 ]
