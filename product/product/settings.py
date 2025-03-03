3aa84e5d7846b212e5b32ce7820f2b584ea64e51
InnaLik <liksakova@list.ru>
2025-03-03 23:36:36 +0300

    change settings

--- product/product/settings.py
+++ product/product/settings.py
@@ -153,3 +153,18 @@ INTERNAL_IPS = [
     "127.0.0.1",
     # ...
 ]
+
+SYSTEM_ENV = os.getenv('SYSTEM_ENV')
+if SYSTEM_ENV == 'GITHUB_WORKFLOW':
+    DEBUG = True
+    SECRET_KEY = 'TESTING_KEY'
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'github_actions',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+        }
+    }
