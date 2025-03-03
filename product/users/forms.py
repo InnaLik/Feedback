9c8d6ba38b02f3b90ca68cf69f95d88868f3137a
InnaLik <liksakova@list.ru>
2025-03-04 00:27:32 +0300

    change db linter postgre

--- product/users/forms.py
+++ product/users/forms.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.forms import AuthenticationForm
-from users.models import User
+# from users.models import User
 
 
 # этот класс по сути нужен, чтобы применять валидаторы на вводимые данные
-class UserLoginForm(AuthenticationForm):
-
-    class Meta:
-        # с какой моделью будем работать
-        model = User
+# class UserLoginForm(AuthenticationForm):
+#
+#     class Meta:
+#         # с какой моделью будем работать
+#         model = User
