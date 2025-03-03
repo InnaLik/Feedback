3717430ad99724b6d3c99379a51338679097acd5
InnaLik <liksakova@list.ru>
2025-03-04 00:25:14 +0300

    change db linter postgre

--- product/product/settings.py
+++ product/product/settings.py
@@ -169,7 +169,7 @@ if SYSTEM_ENV == 'GITHUB_WORKFLOW':
         }
     }
     STATICFILES_DIRS = []
-    AUTH_USER_MODEL = 'users.User'
+    AUTH_USER_MODEL = 'auth.User'
     INSTALLED_APPS.remove('debug_toolbar')
     MIDDLEWARE.remove('debug_toolbar.middleware.DebugToolbarMiddleware')
     DATABASES['default']['DISABLE_SERVER_SIDE_CURSORS'] = True
