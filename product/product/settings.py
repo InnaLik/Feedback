9e8b2ca523c5ca33f12a1f831b923a4ccbe80b3f
InnaLik <liksakova@list.ru>
2025-03-05 00:22:49 +0300

    add wait

--- product/product/settings.py
+++ product/product/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -46,7 +47,6 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'main',
     'goods',
-    'users',
 ]
 
 MIDDLEWARE = [
