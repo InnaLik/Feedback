e67e4b305d07ce86e0b52d5e1200419c5a652c98
InnaLik <liksakova@list.ru>
2025-03-06 01:16:08 +0300

    add tests

--- product/main/tests_main.py
+++ product/main/tests_main.py
@@ -5,26 +5,20 @@ from django.urls import reverse
 @pytest.mark.django_db
 class TestMain:
 
-    def test_success_main_page(self, client):
-        path = reverse('main:index')
+    @pytest.mark.parametrize(
+        "path_name, template, title, content",
+        [
+            ("main:index", "main/index.html", "Home - Главная", "Отзывы о продуктах"),
+            ("main:about", "main/about.html", "Home - обо мне", "Обо мне"),
+        ],
+    )
+    def test_success_pages(self, client, path_name, template, title, content):
+        path = reverse(path_name)
         response = client.get(path)
         assert response.status_code == 200
-        assert response.templates[0].name == 'main/index.html'
-        assert response.context['title'] == "Home - Главная"
-        assert response.context['content'] == "Отзывы о продуктах"
+        assert response.templates[0].name == template
+        assert response.context['title'] == title
+        assert response.context['content'] == content
 
-        assert "Home - Главная" in response.content.decode()
-        assert "Отзывы о продуктах" in response.content.decode()
-
-    def test_success_about(self, client):
-        path = reverse('main:about')
-        response = client.get(path)
-        assert response.status_code == 200
-        assert response.templates[0].name == 'main/about.html'
-        assert response.context['title'] == 'Home - обо мне'
-        assert response.context['content'] == 'Обо мне'
-        assert response.context['text_on_page'] == 'Программист мечтатель'
-
-        assert "Home - обо мне" in response.content.decode()
-        assert "Обо мне" in response.content.decode()
-        assert "Программист мечтатель" in response.content.decode()
+        assert title in response.content.decode()
+        assert content in response.content.decode()
--- product/users/tests_users.py
+++ product/users/tests_users.py
@@ -1,3 +1,22 @@
-from django.test import TestCase  # noqa
+import pytest
+from django.urls import reverse
 
-# Create your tests here.
+
+@pytest.mark.django_db
+class TestUsers:
+    @pytest.mark.parametrize(
+        "path_name, template, title",
+        [
+            ("user:login", "users/login.html", "Авторизация"),
+            ("user:registration", "users/registration.html", "Регистрация"),
+            ("user:profile", "users/profile.html", "Кабинет"),
+        ],
+    )
+    def test_success_pages(self, client, path_name, template, title):
+        path = reverse(path_name)
+        response = client.get(path)
+        assert response.status_code == 200
+        assert response.templates[0].name == template
+        assert response.context['title'] == title
+
+        assert title in response.content.decode()
