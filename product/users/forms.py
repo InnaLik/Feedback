c64fec05207be7cd9a4cc476d72eeb0dd07df3e6
InnaLik <liksakova@list.ru>
2025-03-06 01:02:44 +0300

    form username only frontend

--- product/users/forms.py
+++ product/users/forms.py
@@ -5,25 +5,28 @@ from users.models import User
 
 # этот класс по сути нужен, чтобы применять валидаторы на вводимые данные
 class UserLoginForm(AuthenticationForm):
-
-    username = forms.CharField(
-        widget=forms.TextInput(
-            attrs={
-                "autofocus": True,
-                "class": "form-control",
-                "placeholder": "Введите ваше имя пользователя",
-            }
-        )
-    )
-    password = forms.CharField(
-        widget=forms.PasswordInput(
-            attrs={
-                "autocomplete": "current-password",
-                "class": "form-control",
-                "placeholder": "Введите ваш пароль",
-            }
-        ),
-    )
+    username = forms.CharField()
+    password = forms.CharField()
+    # username = forms.CharField(
+    #     label='Имя пользователя',
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             "autofocus": True,
+    #             "class": "form-control",
+    #             "placeholder": "Введите ваше имя пользователя",
+    #         }
+    #     ),
+    # )
+    # password = forms.CharField(
+    #     label='Пароль',
+    #     widget=forms.PasswordInput(
+    #         attrs={
+    #             "autocomplete": "current-password",
+    #             "class": "form-control",
+    #             "placeholder": "Введите ваш пароль",
+    #         }
+    #     ),
+    # )
 
     class Meta:
         # с какой моделью будем работать
