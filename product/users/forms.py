f675b8eb277f9ad6bcbc299a930369597d8009f6
InnaLik <liksakova@list.ru>
2025-03-07 00:33:26 +0300

    change form

--- product/users/forms.py
+++ product/users/forms.py
@@ -3,30 +3,9 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from users.models import User
 
 
-# этот класс по сути нужен, чтобы применять валидаторы на вводимые данные
 class UserLoginForm(AuthenticationForm):
     username = forms.CharField()
     password = forms.CharField()
-    # username = forms.CharField(
-    #     label='Имя пользователя',
-    #     widget=forms.TextInput(
-    #         attrs={
-    #             "autofocus": True,
-    #             "class": "form-control",
-    #             "placeholder": "Введите ваше имя пользователя",
-    #         }
-    #     ),
-    # )
-    # password = forms.CharField(
-    #     label='Пароль',
-    #     widget=forms.PasswordInput(
-    #         attrs={
-    #             "autocomplete": "current-password",
-    #             "class": "form-control",
-    #             "placeholder": "Введите ваш пароль",
-    #         }
-    #     ),
-    # )
 
     class Meta:
         # с какой моделью будем работать
