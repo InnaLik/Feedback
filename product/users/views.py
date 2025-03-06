17d3c988a4a9d54f7964dfcaefba6d3b5d971e4b
InnaLik <liksakova@list.ru>
2025-03-06 22:19:25 +0300

    add registration

--- product/users/views.py
+++ product/users/views.py
@@ -1,8 +1,9 @@
 from django.contrib import auth
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 # Create your views here.
@@ -28,7 +29,19 @@ def login(request):
 
 
 def registration(request):
-    context = {'title': 'Регистрация'}
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            # заносим данные в бд
+            form.save()
+            # сразу войдем под пользователем
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
+
+    context = {'title': 'Регистрация', 'form': form}
 
     return render(request, 'users/registration.html', context)
 
@@ -39,4 +52,6 @@ def profile(request):
     return render(request, 'users/profile.html', context)
 
 
-def logout(request): ...
+def logout(request):
+    auth.logout(request)
+    return redirect(reverse('main:index'))
