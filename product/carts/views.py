5a6663c2a8068cf48509e47fc663d30d8f59a83b
InnaLik <liksakova@list.ru>
2025-03-12 23:25:51 +0300

    add docs

--- product/carts/views.py
+++ product/carts/views.py
@@ -1,13 +1,13 @@
 # from django.shortcuts import render
 
 
-# Create your views here.
 def cart_add(request, product_id):
-    pass
+    """Добавляет товар в корзину."""
 
 
 def cart_change(request, product_id):
     pass
 
 
-def cart_remove(request, product_id): ...
+def cart_remove(request, product_id):
+    """Удаляет товар из корзины."""
