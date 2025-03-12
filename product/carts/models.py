0a016aa7e5255e1053a5fdede267e154af5e5eb6
InnaLik <liksakova@list.ru>
2025-03-13 01:59:13 +0300

    change linter

--- product/carts/models.py
+++ product/carts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from goods.models import Products
+
 from product import settings
 
 
@@ -19,7 +20,9 @@ class CartQueryset(models.QuerySet):
 
 class Cart(models.Model):
 
-    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True, verbose_name='Пользователь')
+    user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True, verbose_name='Пользователь'
+    )
     product = models.ForeignKey(to=Products, on_delete=models.CASCADE, verbose_name='Товар')
     quantity = models.PositiveSmallIntegerField(default=0, verbose_name='Количество')
     session_key = models.CharField(max_length=32, blank=True, null=True)
