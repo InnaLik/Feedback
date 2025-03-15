43b07ecdc027524816b893bbe91f93742137a030
InnaLik <liksakova@list.ru>
2025-03-15 23:00:04 +0300

    add docs

--- product/carts/templatetags/carts_tags.py
+++ product/carts/templatetags/carts_tags.py
@@ -1,8 +1,8 @@
+from carts.models import Cart
 from django import template
 
-from carts.models import Cart
-
 register = template.Library()
+
 
 @register.simple_tag()
 def user_carts(request):
--- product/users/views.py
+++ product/users/views.py
@@ -1,4 +1,3 @@
-from carts.models import Cart
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
