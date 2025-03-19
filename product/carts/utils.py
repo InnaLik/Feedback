33f7af0e7e7e1a86dece747ab63b0e5e83e3a855
InnaLik <liksakova@list.ru>
2025-03-20 00:30:53 +0300

    add change cart with js

--- product/carts/utils.py
+++ product/carts/utils.py
@@ -2,6 +2,7 @@ from carts.models import Cart
 
 
 def get_user_carts(request):
+    """Получение корзин пользователя."""
     if request.user.is_authenticated:
         carts = Cart.objects.filter(user=request.user)
         return carts
