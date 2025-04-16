c3764ae51339dfd14395dc135736b27eef72fb69
InnaLik <liksakova@list.ru>
2025-04-17 00:02:41 +0300

    add CreateOrderView

--- product/orders/views.py
+++ product/orders/views.py
@@ -1,75 +1,81 @@
 from carts.models import Cart
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import ValidationError
 from django.db import transaction
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views.generic import FormView
 from orders.forms import CreateOrderForm
 from orders.models import Order, OrderItem
 
 
-@login_required
-def create_order(request):
-    """Создание заказа."""
-    if request.method == 'POST':
-        form = CreateOrderForm(data=request.POST)
-        if form.is_valid():
-            try:
-                # атомарная транзакция, все ниже происходит в рамках одной транзакции,
-                # коммит создается только, если нет ошибок
-                with transaction.atomic():
-                    # пользователь
-                    user = request.user
-                    # сохраняем все корзины пользователя
-                    cart_items = Cart.objects.filter(user=user)
-                    # если корзины имеются
-                    if cart_items.exists():
-                        # Создаем заказ по форме
-                        order = Order.objects.create(
-                            user=user,
-                            phone_number=form.cleaned_data['phone_number'],
-                            requires_delivery=form.cleaned_data['requires_delivery'],
-                            delivery_address=form.cleaned_data['delivery_address'],
-                            payment_on_get=form.cleaned_data['payment_on_get'],
-                        )
-                        # Создать заказанные товары
-                        for cart_item in cart_items:
-                            product = cart_item.product
-                            name = cart_item.product.name
-                            price = cart_item.product.price
-                            quantity = cart_item.quantity
+class CreateOrderView(LoginRequiredMixin, FormView):
+    template_name = 'orders/create_order.html'
+    form_class = CreateOrderForm
+    success_url = reverse_lazy("user:profile")
 
-                            if product.quantity < quantity:
-                                raise ValueError(
-                                    f'Недостаточное количество товара на складе {name} в наличии {product.quantity}'
-                                )
-                            # записываем в бд информацию по проданным товарам
-                            OrderItem.object.create(
-                                order=order, product=product, name=name, price=price, quantity=quantity
+    def get_initial(self):
+        """Предзаполнение полей."""
+        initial = super().get_initial()
+        initial['first_name'] = self.request.user.first_name
+        initial['last_name'] = self.request.user.last_name
+        return initial
+
+    def form_valid(self, form):
+        try:
+            # атомарная транзакция, все ниже происходит в рамках одной транзакции,
+            # коммит создается только, если нет ошибок
+            with transaction.atomic():
+                # пользователь
+                user = self.request.user
+                # сохраняем все корзины пользователя
+                cart_items = Cart.objects.filter(user=user)
+                # если корзины имеются
+                if cart_items.exists():
+                    # Создаем заказ по форме
+                    order = Order.objects.create(
+                        user=user,
+                        phone_number=form.cleaned_data['phone_number'],
+                        requires_delivery=form.cleaned_data['requires_delivery'],
+                        delivery_address=form.cleaned_data['delivery_address'],
+                        payment_on_get=form.cleaned_data['payment_on_get'],
+                    )
+                    # Создать заказанные товары
+                    for cart_item in cart_items:
+                        product = cart_item.product
+                        name = cart_item.product.name
+                        price = cart_item.product.price
+                        quantity = cart_item.quantity
+
+                        if product.quantity < quantity:
+                            raise ValueError(
+                                f'Недостаточное количество товара на складе {name} в наличии {product.quantity}'
                             )
-                            product.quantity -= quantity
-                            product.save()
-                        # удаляем эти товары из корзин пользователя, так как заказ оформлен и
-                        # далее работаем уже с другой табличкой в бд
-                        # чтобы не было такого, что пользователь заказал корзину, заходит в корзину,
-                        # а там снова эти товары тусуются
-                        cart_items.delete()
+                        # записываем в бд информацию по проданным товарам
+                        OrderItem.object.create(order=order, product=product, name=name, price=price, quantity=quantity)
+                        product.quantity -= quantity
+                        product.save()
+                    # удаляем эти товары из корзин пользователя, так как заказ оформлен и
+                    # далее работаем уже с другой табличкой в бд
+                    # чтобы не было такого, что пользователь заказал корзину, заходит в корзину,
+                    # а там снова эти товары тусуются
+                    cart_items.delete()
 
-                        messages.success(request, message="Заказ оформлен")
-                        # возвращаем пользователя на его профиль
-                        return redirect('user:profile')
-            except ValidationError as e:
-                messages.success(request, str(e))
-                # если ошибка валидации, то заново пользователю отображаем страницу с корзиной
-                return redirect('cart:order')
+                    messages.success(self.request, message="Заказ оформлен")
+                    # возвращаем пользователя на его профиль
+                    return redirect('user:profile')
+        except ValidationError as e:
+            messages.success(self.request, str(e))
+            # если ошибка валидации, то заново пользователю отображаем страницу с корзиной
+            return redirect('cart:order')
 
-    else:
-        # изначальные данные
-        initial = {'first_name': request.user.first_name, 'last_name': request.user.last_name}
-        # создаем пустую форму, если пользователь только зашел в оформление заказа, и заполняем предзаполненные данные,
-        # так как пользователь у нас уже авторизован
-        form = CreateOrderForm(initial=initial)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Оформление заказа'
+        context['order'] = True
+        return context
 
-    context = {"title": 'Оформление заказа', "form": form, "order": True}
-
-    return render(request, 'orders/create_order.html', context)
+    def form_invalid(self, form):
+        messages.error(self.request, "Заказ не оформлен")
+        return redirect('orders:create_order')
