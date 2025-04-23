56af87216b8f8cb626eff761fb5e7f1c1eeecd5a
InnaLik <liksakova@list.ru>
2025-04-23 20:10:40 +0300

    cached class

--- product/main/urls.py
+++ product/main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 from main import views
 
 # чтобы работал namespace
 app_name = 'main'
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('', cache_page(60)(views.IndexView.as_view()), name='index'),
     path('about/', views.AboutView.as_view(), name='about'),
 ]
