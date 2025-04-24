59004d300bde601966f8784911d00a207c19cf5a
InnaLik <liksakova@list.ru>
2025-04-25 00:55:10 +0300

    change settings installed apps

--- product/product/settings.py
+++ product/product/settings.py
@@ -25,7 +25,7 @@ ALLOWED_HOSTS = ["*"]
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
-    'users',
+    'users.apps.UsersConfig',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
